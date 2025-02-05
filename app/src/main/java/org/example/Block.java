f8dd359727628cf4ce31b2084a192b081df55d90
Thilan <thil4n@gmail.com>
2025-02-05 22:16:01 +0530

    fix logic issues

---
+++ app/src/main/java/org/example/Block.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import java.time.Instant;
+
+public class Block {
+    private int index;
+    private String data;
+    private int nonce;
+    private int difficulty;
+    private long timestamp;
+    private String creator;
+    private String signature;
+    private String hashString;
+    private String prevHashString;
+
+    // Constructor
+    public Block(int index, String data, int difficulty, String prevHashString) {
+        this.index = index;
+        this.data = data;
+        this.difficulty = difficulty;
+        this.prevHashString = prevHashString;
+        // this.creator = creator;
+        // this.signature = signature;
+        this.timestamp = Instant.now().toEpochMilli(); // Set current timestamp
+        this.hashString = calculateHash(); // Calculate initial hash
+
+        mineBlock(); // Mine the block
+    }
+
+    // Hash calculation
+    public String calculateHash() {
+        String dataToHash = index + data + nonce + difficulty + timestamp + creator + signature + prevHashString;
+        this.hashString = StringUtil.applySha256(dataToHash);
+        return hashString;
+    }
+
+    // Mining process with Proof-of-Work
+    public void mineBlock() {
+        String target = "0".repeat(difficulty);
+        nonce = 0;
+        hashString = calculateHash();
+
+        while (!hashString.startsWith(target)) {
+            nonce++;
+            hashString = calculateHash();
+        }
+
+        System.out.println("Block Mined! Hash: " + hashString);
+    }
+
+    // Getters
+    public int getIndex() {
+        return index;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public String getHashString() {
+        return hashString;
+    }
+
+    public String getPrevHashString() {
+        return prevHashString;
+    }
+}
---
+++ app/src/main/java/org/example/Chain.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Chain {
+    private final int difficulty = 4;  // Adjust for mining difficulty
+    private final ArrayList<Block> blockchain = new ArrayList<>();
+
+    public Chain() {
+        // Create the Genesis Block
+        blockchain.add(new Block(0, "Genesis Block", difficulty, "0"));
+    }
+
+    public Block getLatestBlock() {
+        return blockchain.get(blockchain.size() - 1);
+    }
+
+    public void addBlock(String data) {
+        Block newBlock = new Block(blockchain.size(),data,  difficulty,  getLatestBlock().getHashString());
+        blockchain.add(newBlock);
+    }
+
+    public boolean isChainValid() {
+        for (int i = 1; i < blockchain.size(); i++) {
+            Block currentBlock = blockchain.get(i);
+            Block previousBlock = blockchain.get(i - 1);
+
+            if (!currentBlock.getHashString().equals(currentBlock.calculateHash())) {
+                System.out.println("Block " + i + " has been tampered!");
+                return false;
+            }
+
+            if (!currentBlock.getPrevHashString().equals(previousBlock.getHashString())) {
+                System.out.println("Block " + i + " is not linked properly!");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void printChain() {
+        for (Block block : blockchain) {
+            System.out.println("Block #" + block.getIndex());
+            System.out.println("Data: " + block.getData());
+            System.out.println("Previous Hash: " + block.getPrevHashString());
+            System.out.println("Current Hash: " + block.getHashString());
+            System.out.println("------------------------");
+        }
+    }
+
+    public static void main(String[] args) {
+        Chain chain = new Chain();
+
+        System.out.println("Mining block 1...");
+        chain.addBlock("Transaction 1");
+
+        System.out.println("Mining block 2...");
+        chain.addBlock("Transaction 2");
+
+        System.out.println("Mining block 3...");
+        chain.addBlock("Transaction 3");
+
+        chain.printChain();
+        System.out.println("Blockchain valid: " + chain.isChainValid());
+    }
+}
